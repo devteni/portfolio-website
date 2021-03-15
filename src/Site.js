05391abdc1c6b0b98be6be759cae09786b43bf97
Omoteniola-dev <fatunmbiteniola001@gmail.com>
2021-03-15 19:18:17 +0100

    added a Site component

--- src/Site.js
+++ src/Site.js
@@ -1,12 +1,24 @@
-
 import './App.css';
+
+var i = 0;
+var txt = "Developer, Writer";
+var speed = 50;
+var anim = document.getElementById("demo")
+
+function typeWriter() {
+  if (i < txt.length) {
+    anim.innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
 
 function Site() {
   return (
     <div>
       <div id="About" className="text-gray-300 mt-52 text-center h-screen">
         <p className="text-3xl font-bold lg:text-5xl">Hi, I am Teniola</p>
-        <p className="text-xl lg:text-2xl"><code>Developer,</code> Writer</p>
+        <p className="text-xl lg:text-2xl" id="demo" onLoad={typeWriter}></p>
       </div>
       
     </div>
