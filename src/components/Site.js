405c71a7e3f7e64370e1e8662cbdc53614144629
Teniola Fatunmbi <fatunmbiteniola001@gmail.com>
2021-05-08 14:57:02 +0100

    Changed the text

--- src/components/Site.js
+++ src/components/Site.js
@@ -6,8 +6,9 @@ function Site() {
     <div>
       <div id="About" className="text-gray-300 mt-52 text-center h-screen" data-aos="fade-up">
         <p className="text-3xl font-bold lg:text-5xl">Hi, I am Teniola</p>
-        <p className="text-xl lg:text-2xl" id="demo" data-aos="fade-right">Software engineer and <p>writer.</p></p>
+        <p className="text-xl lg:text-2xl" id="demo" data-aos="fade-right"><code>Developer </code>and <p>writer.</p></p>
       </div>
+      <p>SVG image here</p>
       
     </div>
   );
