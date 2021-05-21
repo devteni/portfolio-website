68724eb1fa36965cb5729be407a4261acfc2664a
Teniola <fatunmbiteniola001@gmail.com>
2021-05-21 23:10:13 +0100

    added live and repo links

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Project = ({id, desc, tags}) => {
+const Project = ({id, desc, tags, live, repo}) => {
     return (
          <div className="lg:h-1/3 lg:w-1/3 m-5 p-4 w-full shadow-lg lg:hover:bg-gray-300 
                     hover:shadow-md bg-gray-100 justify-center lg:shadow-xl rounded-xl" key={id}>
@@ -13,8 +13,8 @@ const Project = ({id, desc, tags}) => {
                 }
              </div>
              <section className="text-right">
-             <a href="" className="m-1"> Live </a>
-             <a href="">Repo</a>
+             <a href={live} className="m-1"> Live </a>
+             <a href={repo}>Repo</a>
              </section>
         </div>       
             )
