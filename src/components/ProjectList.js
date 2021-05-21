686097a1aad2fb945e507ee0732783df18c0c7f2
Teniola <fatunmbiteniola001@gmail.com>
2021-05-21 22:36:51 +0100

    minor mods

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,9 +2,9 @@ import React from "react";
 import Project from "./Project"
 const ProjectList = ({data}) => {
     return(
-        <div className="h-full border-gray-900" id="Projects" data-aos="fade-up">
+        <div className="mt-10 border-gray-900" id="Projects" data-aos="fade-up">
         <p className="transition-1000 text-center mb-4 font-bold lg:font-extrabold text-3xl antialiased">PROJECTS</p>   
-            <div className="flex flex-wrap flex-row justify-center lg:flex-row flex-wrap ma-3">
+            <div className="flex flex-wrap flex-row justify-center lg:flex-row flex-wrap">
                 {
                 data.map((project, i) => {
                     return(<Project key={i}
