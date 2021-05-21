62028ae90341b49eed11d9371cc768d4f7b6657e
Teniola <fatunmbiteniola001@gmail.com>
2021-05-21 22:34:37 +0100

    code refactored

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,67 +1,30 @@
 import React, {Component} from "react";
 import './App.css';
-import Site from '../components/Site';
+import Home from "./Home"
 import Navbar from '../components/Navbar'
-import About from '../components/About';
-import ProjectList from "../components/ProjectList";
 import Links from "../components/Links"
-import AOS from 'aos';
+
 import 'aos/dist/aos.css';
-import {BrowserRouter as Router, } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Contact from "../components/Contact";
 
 
 class App extends Component{
-  constructor(){
-    super();
-    AOS.init({
-      duration: 1200
-    })
-    this.state = {
-        Services: [
-          {
-          "id": 1,
-          "image": null, 
-          "desc": "IT company website - Kebisglobal",
-          "tags": ["React", "Tailwind", "Javascript"]
-        },
-        {
-          "id":2,
-          "image": null, 
-          "desc": "Pdf downloader",
-          "tags": ["Python"]
-        },
-        {
-          "id": 3,
-          "image": null, 
-          "desc": "Employee-info-manager app",
-          "tags": ["React", "Tailwind", "Javascript"]
-        },
-        {
-          "id": 4,
-          "image": null, 
-          "desc": "Portfolio website",
-          "tags": ["React", "Tailwind", "Javascript"]
-        }
-        ]
-    }
-}
-
-  componentWillReceiveProps(){
-    AOS.refresh()
-  }
   render(){
-    const { Services } = this.state;
     return (
       <Router>
         <div >
           <Navbar />
           <div className="lg:p-32 p-5">
-            <Site />
-            <About />
-            <ProjectList data ={Services}/>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/contact" component={Contact} />
+            </Switch>
           </div>
           <Links />
-       </div> 
+       
+       </div>
+       
       </Router>
     
   );
