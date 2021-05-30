6eff0e34705e8760ac1a674f3d51b0552549b9b4
Teniola <fatunmbiteniola001@gmail.com>
2021-05-30 19:24:18 +0100

    modified the image spacing and size

--- src/components/Site.js
+++ src/components/Site.js
@@ -25,14 +25,14 @@ const social = [
 function Site() {
   return (
     <>
-      <div id="About" className="text-gray-300 text-center" data-aos="fade-up">
+      <div id="About" className="text-gray-300 text-center mt-16" data-aos="fade-up">
         <img src={Teni} alt="Teniola Fatunmbi" className="rounded-full lg:w-1/5 w-3/5 lg:mx-32 mx-10 my-2"/>
         <p className="text-3xl font-bold lg:text-5xl">Hi, I am Teniola</p>
         <p className="text-xl lg:text-2xl" id="demo" data-aos="fade-right"><code>Developer </code>and <p>writer.</p></p>
       <div className="flex flex-row flex-wrap justify-center">
         {
           social.map((link, i) => {
-            return <a href={link.link}><img src={link.svg} alt={link.alt} className="m-4" key={i}/></a>
+            return <a href={link.link}><img src={link.svg} width="30" height="30" alt={link.alt} className="m-4" key={i}/></a>
           })
         }
       </div>
