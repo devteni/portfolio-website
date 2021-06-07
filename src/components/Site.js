a2888530e76340ff6cccc03ff6d2808ac9ac371a
Teniola <fatunmbiteniola001@gmail.com>
2021-06-07 20:16:26 +0100

    modified the styling

--- src/components/Site.js
+++ src/components/Site.js
@@ -3,6 +3,7 @@ import Teni from "../avatar.jpg"
 import github from "../svg/github.svg"
 import twitter from "../svg/twitter.svg"
 import linkedin from "../svg/linkedin.svg"
+import { Link } from 'react-router-dom';
 
 const social = [
   {
@@ -25,19 +26,25 @@ const social = [
 function Site() {
   return (
     <>
-      <div id="About" className="text-gray-300 text-center mt-16" data-aos="fade-up">
-        <img src={Teni} alt="Teniola Fatunmbi" className="rounded-full lg:w-1/5 w-3/5 lg:mx-32 mx-10 my-2"/>
-        <p className="text-3xl font-bold lg:text-5xl">Hi, I am Teniola</p>
-        <p className="text-xl lg:text-2xl" id="demo" data-aos="fade-right"><code>Developer </code>and <p>writer.</p></p>
-      <div className="flex flex-row flex-wrap justify-center">
-        {
-          social.map((link, i) => {
-            return <a href={link.link}><img src={link.svg} width="30" height="30" alt={link.alt} className="m-4" key={i}/></a>
-          })
-        }
+    <img src={Teni} alt="Teniola Fatunmbi" className="rounded-full lg:w-0.5 w-2/5 lg:my-32 mx-10 mt-14 lg:my-4"/>
+      <div id="About" className="flex flex-wrap text-gray-300 text-center text-white lg:w-3/4" data-aos="fade-up">
+        <div className="flex flex-row flex-wrap justify-center mx-10">
+          {
+            social.map((link, i) => {
+              return <a href={link.link}><img src={link.svg} width="30" height="30" alt={link.alt} className="m-4" key={i}/></a>
+            })
+          }
+        </div>
+       <>
+          <p className="text-3xl font-bold lg:text-5xl lg:w-3/5">Hi! I'm Teniola.
+          I <span className="text-gray-500">build stuffs that work on the web</span> and also <span className="text-gray-500">write about them</span>.</p>
+        </>
+        <p className="text-center text-xl border-solid bor border-blue-700 p-3 m-5">
+          <Link to="/contact" >
+              Contact
+          </Link>
+        </p>
       </div>
-      </div>
-      
     </>
   );
 }
