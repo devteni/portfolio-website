7c5eb675c456f992d57f424107bbb270437b911d
devteni <fatunmbiteniola001@gmail.com>
2021-10-22 00:21:30 +0100

    doings: some tweaks in here for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,30 +2,34 @@ import React from "react";
 
 const Label = ({ live, repo }) => {
     return(
-        <section className="text-right font-sm text-sm">
-             <a href={live} target="_blank" rel="noreferrer" className="m-1 hover:text-gray-200"> Live </a>
-             <a href={repo} className="hover:text-gray-200">Repo</a>
+        <section className="text-right font-sm text-sm label">
+             <a href={live} target="_blank" rel="noreferrer" className="m-3 hover:text-gray-200"> Live </a>
+             <a href={repo} className="m-3 hover:text-gray-200">Repo</a>
         </section>
     )
 }
 
-const Project = ({id, title, desc, tags, live, repo}) => {
+const Project = ({id, image, title, desc, tags, live, repo}) => {
     return (
-         <div className="lg:w-1/3 m-5 p-4 w-full shadow-lg
-                    hover:shadow-md bg-gray-500 text-white font-bold justify-center lg:shadow-xl rounded-lg" key={id}>
-             <p className="text-base lg:text-lg px-3 pb-0">{title}</p>
-             <div className="flex justify-end flex-row-reverse flex-wrap p-1">
-                {
-                    tags.map((tag, i) => {
-                        return <span className="p-1 px-2 m-1 bg-gray-400 font-sm text-sm rounded-xl">{tag}</span>
-                    })
-                }
-             </div>
-             <p className="p-2">{desc}</p>
-             <Label live={live} repo={repo}/>
-        </div>       
+        <div className="project h-1/3 lg:w-4/5 w-full my-5" data-aos="zoom-in">
+            <div className="lg:w-2/3 p-2 text-center">
+                <img src={image} alt="" className="h-full"/>
+            </div>
+            <div className="lg:w-1/3 p-4 w-full shadow-lg
+                    hover:shadow-md text-white font-bold justify-center lg:shadow-xl rounded-lg h-1/3" key={id}>
+                <p className="text-base lg:text-lg px-3 pb-0">{title}</p>
+                <div className="flex justify-end flex-row-reverse flex-wrap p-1">
+                    {
+                        tags.map((tag, i) => {
+                            return <span className="p-1 px-2 m-1 bg-gray-400 font-sm text-sm rounded-xl">{tag}</span>
+                        })
+                    }
+                </div>
+                <p className="p-2">{desc}</p>
+                <Label live={live} repo={repo}/>
+            </div>  
+        </div>
             )
 }
-        
 
 export default Project;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Project from "./Project"
+import Project from "./Project";
+
 const ProjectList = ({data}) => {
     return(
         <div className="mt-10 border-gray-900" id="Projects" data-aos="fade-up">
         <p className="transition-1000 text-center mb-4 font-bold lg:font-extrabold text-3xl antialiased text-white">Projects</p>   
-            <div className="flex flex-wrap flex-row justify-center lg:flex-row flex-wrap">
+            <div className="flex flex-wrap flex-row lg:justify-center lg:flex-row flex-wrap">
                 {
                 data.map((project, i) => {
                     return(<Project key={i}
@@ -18,7 +19,6 @@ const ProjectList = ({data}) => {
                 })
             }
             </div>    
-            <p className="p-3 bg-gray-600 mx-20 text-center lg:float-right mr-48 w-1/3 lg:w-24 rounded-lg hover:bg-gray-700"><a href="https://github.com/Omoteniola-dev" rel="noreferrer" target="_blank">View more</a></p>
         </div>
     )
 }
