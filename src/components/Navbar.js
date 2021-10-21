5d1f547bb432bfc6d8993e637e2e07007517ab19
devteni <fatunmbiteniola001@gmail.com>
2021-10-21 19:24:45 +0100

    doings: cleaned up the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import AOS from "aos";
 import React from "react";
-import Logo from "../logo.png"
+// import Logo from "../logo.png"
 import { Link } from "react-router-dom";
 
 AOS.init({
     duration: 100
 })
+
 
 const Navbar = () => {
       return (
@@ -13,21 +14,21 @@ const Navbar = () => {
             <nav className="fixed w-screen lg:px-10 sm:px-10 px-2 sm:flex justify-between border-gray-600 z-10 bg-gray" style={{"background": "#282c34"}} data-aos="fade-left">
                 <div className="flex px-4 justify-between sm:py-0 pb-1">
                     <img
-                     src={Logo}
-                    alt="Logo"
+                     src=""
+                    alt="..."
                     className="h-12 m-3"
                     />
                 </div>
         
-            <ul className="hidden sm:flex cursor-pointer" id="navContent">
-                <li className="py-5 px-5 hover:text-blue-600 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent text-blue-700" data-aos="fade-up">
+            <ul className="hidden sm:flex cursor-pointer text-white" id="navContent">
+                <li className="py-5 px-5 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent" data-aos="fade-up">
                     <Link to="/">Home</Link>
                 </li>
-                <li className="py-5 px-5 hover:text-blue-600 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent text-blue-700" data-aos="fade-up">
-                    <a href="https://tenifatunmbi.hashnode.dev" target="_blank" rel="noreferrer">Blog</a>
+                <li className="py-5 px-5 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent" data-aos="fade-up">
+                    <Link to="/about">About</Link>
                 </li>
-                <li className="py-5 px-5 hover:text-blue-600 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent text-blue-700" data-aos="fade-up">
-                    <Link to="/contact">Get in touch</Link>
+                <li className="py-5 px-5 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent" data-aos="fade-up">
+                    <a href="https://devteni.hashnode.dev" target="_blank" rel="noreferrer">Blog</a>
                 </li>
             </ul>
             </nav>
