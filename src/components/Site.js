ae395d5071a92cb40168413c98a9a6ad06e623fa
devteni <fatunmbiteniola001@gmail.com>
2021-12-22 04:19:01 +0100

    [feat]: fixed home layout

--- src/components/Site.js
+++ src/components/Site.js
@@ -5,14 +5,14 @@ function Site() {
   return (
     <>
       <div id="about" className="flex flex-wrap justify-center text-gray-300 text-center text-white lg:w-3/4 
-      lg:mt-32 mx-10 mt-14 p-8" data-aos="fade-up">
-        <div className="tracking-widest">
-          <p className="text-left font-bold lg:w-3/5"><span className="text-3xl lg:text-4xl">Hi! I'm Teniola.</span><br />
+      lg:mt-32 mx-8 mt-14 p-3" data-aos="fade-up">
+        <div className="tracking-widest leading-9">
+          <p className="text-left font-bold lg:w-3/5"><span className="text-2xl lg:text-4xl">Hi!<br /> I'm Teniola.</span><br />
           I <span className="text-gray-500">build stuffs that work on computers</span> and also <span className="text-gray-500">write about them</span>.
           </p>
           <p className="" data-aos="fade-right">
             <Link to="/contact" className="contact-btn">
-                Contact <i className="fas fa-envelope env"></i>
+                Say Hi <i className="fas fa-envelope env"></i>
             </Link>
           </p>
         </div>
