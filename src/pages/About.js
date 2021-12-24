96080a15464af7ad5f4701846457eea51746a342
devteni <fatunmbiteniola001@gmail.com>
2021-12-24 17:57:08 +0100

    [feat]: implemented layout for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,22 +4,33 @@ import React from "react";
 const skills = [ "HTML & CSS", "JAVASCRIPT", "REACTJS", "NODEJS", "MONGODB", "PYTHON", "GIT"]
 const About = () => {
     return(
-            <div className="h-screen text-center p-5 mt-12">
-                <div className="w-2/5 static-abt border-2 border-right">
-                    <section className="text-sm p-5 object-center">
+            <div className="md:grid md:grid-cols-2 md:gap-2 h-screen p-5">
+                <div className="invisible md:visible border-r-2">
+                    <section className="text-sm p-5 object-center ">
                         You don't need a great work, you need a rarified approach to your work -
                         <br/> <i>Cal Newport</i>
                     </section>
                 </div>
 
-                <div className="lg:px-5 lg:object-right text-white overscroll-auto">
-                    <p className="">Teniola? </p>
-                    <span>Skills</span>
-                    <section>
-                        <ul>
-                            <li>Reactjs</li>
-                            <li>Nodejs</li>
-                        </ul>
+                <div className="lg:px-5 text-center text-white overscroll-auto">
+                    <section id="background">
+                        <p className="capitalize hover:border-b-1 text-gray-500 italic">background</p>
+                        <article className="p-3">
+                            I'm a computer science undergraduate with a roll-up-your-sleeves attitude
+                            to gaining and employing tech skills in the real world. 
+                            
+                            <br /><br />    
+                            A backend engineer by interest, but I dabble in frontend every now and then
+                            'cos I've got a knack for UIs. 
+
+                            I am always open to every opportunity
+                            available to build real-world software solutions.
+                        </article>
+                    </section>
+                    <section id="skills">
+                        <article>
+                            I'd highlight my skills
+                        </article>
                     </section>
                 </div>
                 
