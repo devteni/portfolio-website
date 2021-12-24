d4744e665693802698927fc7f76a85137a5fca51
devteni <fatunmbiteniola001@gmail.com>
2021-12-24 21:28:57 +0100

    [feat]: fixed navbar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
 
       return ( 
         <div data-aos="fade-down">
-            <nav className="fixed w-screen lg:px-10 sm:px-10 px-2 sm:flex justify-between border-gray-600 z-10 bg-gray" 
+            <nav className="fixed py-2 w-screen lg:px-10 sm:px-10 px-2 sm:flex justify-between border-gray-600 z-10 bg-gray" 
             style={{
                 "background": "#282c34",
                 "transition": "all 3s ease-out"
@@ -35,10 +35,10 @@ const Navbar = () => {
         
             <ul className="hidden sm:flex cursor-pointer text-white open"
             id="navContent">
-                <li className="py-5 px-5 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent" data-aos="fade-up">
+                <li onClick={handleClick} className="py-5 px-5 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent" data-aos="fade-up">
                     <Link to="/">Home</Link>
                 </li>
-                <li className="py-5 px-5 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent" data-aos="fade-up">
+                <li onClick={handleClick} className="py-5 px-5 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent" data-aos="fade-up">
                     <Link to="/about">About</Link>
                 </li>
                 <li className="py-5 px-5 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent" data-aos="fade-up">
