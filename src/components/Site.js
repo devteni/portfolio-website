6911a65f68d6fff730d5457a19897c65ae1fad3a
devteni <fatunmbiteniola001@gmail.com>
2021-12-24 23:54:50 +0100

    [feat]: modified design for small and medium sized screens

--- src/components/Site.js
+++ src/components/Site.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 function Site() {
   return (
     <>
-      <div id="about" className="h-screen flex flex-wrap justify-center 
+      <div id="about" className="flex flex-wrap justify-center 
       text-gray-300 text-center text-white lg:w-3/4 
-      lg:mt-32 mt-16 mx-8 tracking-widest leading-9">
+      lg:mt-32 mt-14 mx-8 tracking-widest leading-9">
         <div className="text-left">
           <p className="font-bold lg:text-5xl text-3xl">Hi!<br /> I'm Teniola.<br /></p>
           <p className="font-light lg:text-4xl text-xl mt-10" data-aos="fade-right">I <span className="text-gray-500">build stuffs that work on computers</span> and also <span className="text-gray-500">write about them</span>.
